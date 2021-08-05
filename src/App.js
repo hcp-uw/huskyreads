e76a4448197175f50ee1f09d77f1ba546c75a986
Audrey Kho <audreyckho@gmail.com>
2021-08-04 23:07:11 -0400

    basic navbar functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Navbar from "./components/nav/Nav";
+import './index.css';
+import AboutPage from "./pages/about/About";
+import BookStandPage from "./pages/book-stand/BookStand";
+import HomePage from "./pages/home/Home";
+import LoginPage from "./pages/login/Login";
+import SettingsPage from "./pages/settings/Settings";
+
 export default function App() {
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path="/bookstand">
+          <BookStandPage/>
+        </Route>
+        <Route path="/settings">
+          <SettingsPage/>
+        </Route>
+        <Route path="/about">
+          <AboutPage/>
+        </Route>
+        <Route path="/login">
+          <LoginPage/>
+        </Route>
+        <Route path="/">
+          <HomePage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
+
+// hello! this is the root file and the App()
+// component is basically what gets rendered
+// in index.html
