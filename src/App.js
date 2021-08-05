a5662266403e9b938f27f6aafa929e313d93889c
Audrey Kho <audreyckho@gmail.com>
2021-08-04 23:30:24 -0400

    instructions in App.js 2

--- src/App.js
+++ src/App.js
@@ -34,8 +34,14 @@ export default function App() {
 
 // hello! this is the root file and the App() component is basically what gets
 // rendered in index.html file. I've created a basic file structure to start with
-// but please add new folders/files or change things in current pages if needed!
+// but please add new folders/files or change things anywhere if needed!
 // (some pages have some content because i was testing the navbar)
+
+// Main folders you need to know about:
+// - src/pages: where individual pages in our website goes (usually a folder
+//   containing a .js and .css file)
+// - src/components: where we put components that are imported & used in multiple pages
+// - public: where assets go (like images, files, font files, etc)
 
 // To do a browser preview, just type "npm start" into command line and it'll
 // automatically pull up the site in your browser & will also auto update as
