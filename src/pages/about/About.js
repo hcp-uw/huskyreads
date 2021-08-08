c3f6cc28d45701d35d8504908e510baee651f665
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-08-07 22:34:50 -0700

    Small updates to page
    Looks better and more manageable after page resizes

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,3 +3,10 @@ export default function AboutPage() {
     <div>about page</div>
   );
 }
+
+purposebox = document.getElementById("purpose-box");
+purposebox.addEventListener("resize", () => {
+  if (purposebox.style.width === 400) {
+    purposebox.style.flexDirection = "column";
+  }
+})
