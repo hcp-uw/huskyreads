dcd3bfb44612e680e84a027a2e2aa27203c34514
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-08-16 21:36:35 -0700

    Update to Settings

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 
 export default function SettingsPage() {
-  return (
-    <Button color="primary">Hello World</Button>
-  );
+  return ({
+      
+  });
 }
