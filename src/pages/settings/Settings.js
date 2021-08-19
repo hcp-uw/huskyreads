9c9b4fc431b8a6c9d7429112cbb468c73a2ed57f
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-08-18 17:48:55 -0700

    Basic setup for Settings.js

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { render } from 'react-dom/cjs/react-dom.development';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
 
 export default function SettingsPage() {
+  
+  // create some checked function that accesses the endpoint to see if the site
+  // dark mode or not
+
+  // styling
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: purple[300],
+      '&$checked': {
+        color: purple[500],
+      },
+      '&$checked + $track': {
+        backgroundColor: purple[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+  
+
   return(
-    <p>Settings page</p>
+    <div id="container">
+      <p>Settings page</p>
+      <p>Light Mode left, Dark mode right</p>
+      <PurpleSwitch/>
+    </div>
   );
 }
