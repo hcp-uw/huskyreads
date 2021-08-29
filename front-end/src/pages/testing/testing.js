9aa2ebcc52f15ff22a5441a2d8feb21e8e8b6063
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-08-28 23:28:17 -0700

    Update testing.js

--- front-end/src/pages/testing/testing.js
+++ front-end/src/pages/testing/testing.js
@@ -1,6 +1,10 @@
 let axios = require('axios');
 
-window.addEventListener('load', () => {
+let testVar = {};
+
+testfunc = async () => {
     let response = await axios.get("localhost:8000/books/detail/1111111111").catch((err) => console.log(err));
-    console.log(response);
-}); 
+    testVar = response;
+} 
+
+testfun();
