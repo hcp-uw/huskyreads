39175d6d0bfd2b2d173d1b48c1e758d22c0e86d6
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-08-30 10:15:30 -0700

    search test code
    Relocated search testing code into testing.js. Returned about.js to initial state

--- front-end/src/pages/about/About.js
+++ front-end/src/pages/about/About.js
@@ -125,40 +125,8 @@ function aboutPagePureHTML() {
   
 
 export default function AboutPage() {
-
-    // Just testing my searching capabilities
-    const testSearch = async (title, author, genre) => {
-        let fetchURL = "http://localhost:8000/books/search?";
-
-        if (title !== undefined) {
-            fetchURL += `&title=${title}`;
-        }
-        if (author !== undefined) {
-            fetchURL += `&author=${author}`;
-        }
-        
-        if (genre !== undefined) {
-            for (let i = 0; i < genre.length; i++) {
-                fetchURL += `&genre[${i}]=${genre[i]}`;
-            }
-        }
-
-        const response = await axios.get(fetchURL).catch((error) => console.log(error));
-        console.log(response);
-
-
-    }
-  
     const testing = false;
     if (!testing) {
-        testSearch();
         return(aboutPagePureHTML());
     }
 }
-
-
-
-/*
-// not sure if this is the correct syntax but this'll give us a rough start
-
-*/
--- front-end/src/pages/testing/testing.js
+++ front-end/src/pages/testing/testing.js
@@ -1,6 +1,25 @@
-testfunc = async () => {
-    const response = await fetch("localhost:8000/books/detail/1111111111");
-    return response.json();
-} 
+    /*
+    // Just testing my searching capabilities
+    const testSearch = async (title, author, genre) => {
+        let fetchURL = "http://localhost:8000/books/search?";
 
-testfunc().then(data => console.log(data));
+        if (title !== undefined) {
+            fetchURL += `&title=${title}`;
+        }
+        if (author !== undefined) {
+            fetchURL += `&author=${author}`;
+        }
+        
+        if (genre !== undefined) {
+            for (let i = 0; i < genre.length; i++) {
+                fetchURL += `&genre[${i}]=${genre[i]}`;
+            }
+        }
+
+        const response = await axios.get(fetchURL).catch((error) => console.log(error));
+        console.log(response);
+
+    }
+
+    // await testSearch();
+    */
