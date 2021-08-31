51e5da00616b70232df908289281b05cfb4f0048
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-08-31 16:05:00 -0700

    Finished testing folder
    Fully tested axios code within testing/testing.js. Nitty gritty has been resolved, now need to figure out where we must call these fetches and whether to pass them into the pages when they are entered, OR have the pages receive the fetch parameters and allow the pages to fetch from the backend themselves.

--- front-end/src/pages/about/About.js
+++ front-end/src/pages/about/About.js
@@ -5,42 +5,10 @@ import React, { useState, useEffect } from 'react';
 
 export default function AboutPage() {
     
-    const [data, setData] = useState({});
-
-    useEffect(() => {
-        search(undefined, undefined, ["Horror"]);
-    }, []);
-    
-
-    async function search(title, author, genre) {
-        let fetchURL = "http://localhost:8000/books/search?";
-    
-        if (title !== undefined) {
-            fetchURL += `&title=${title}`;
-        }
-        if (author !== undefined) {
-            fetchURL += `&author=${author}`;
-        }
-        
-        if (genre !== undefined) {
-            for (let i = 0; i < genre.length; i++) {
-                fetchURL += `&genre[${i}]=${genre[i]}`;
-            }
-        }
-    
-        const response = await axios.get(fetchURL).catch((error) => console.log(error));
-        setData(response.data);
-    }
-    
     const testing = false;
     if (!testing) {
         console.log(data);
-        return(
-            <div>
-                {aboutPagePureHTML()}
-                <p>{data.books !== undefined && data.books[0].title}</p>
-            </div>
-        );
+        return(aboutPagePureHTML());
     }
 
     function aboutPagePureHTML() {
@@ -166,25 +134,16 @@ export default function AboutPage() {
 
 }
 
-
     /*
+    HUSKY READS AXIOS EXAMPLE WITH HOOKS (useEffect and useState)
+    ----
     const [data, setData] = useState({});
 
     useEffect(() => {
-        async function search(limit, offset) {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).catch((err) => console.log(err));
-            setData(response.data);
-        }
-        search(5, 0);
+        search(undefined, undefined, ["Horror"]);
     }, []);
+    
 
-
-    Within render: <p>{(data.results !== undefined) ? data.results[0].name : "Not yet"}</p>
-
-    */
-
-
-    /*
     async function search(title, author, genre) {
         let fetchURL = "http://localhost:8000/books/search?";
     
@@ -202,8 +161,25 @@ export default function AboutPage() {
         }
     
         const response = await axios.get(fetchURL).catch((error) => console.log(error));
-        return response;
+        setData(response.data);
     }
-    
-    setData(search(undefined, undefined, ["Horror"]));
+
+    Within return statement: <p>{data.books !== undefined && data.books[0].title}</p>
+
+
+    POKEMON API EXAMPLE
+    ---
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        async function search(limit, offset) {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).catch((err) => console.log(err));
+            setData(response.data);
+        }
+        search(5, 0);
+    }, []);
+
+
+    Within render: <p>{(data.results !== undefined) ? data.results[0].name : "Not yet"}</p>
     */
+
--- front-end/src/pages/testing/testing.js
+++ front-end/src/pages/testing/testing.js
@@ -1,23 +1,48 @@
 /*
-import axios from 'axios';
+    HUSKY READS AXIOS EXAMPLE WITH HOOKS (useEffect and useState)
+    ----
+    const [data, setData] = useState({});
 
-const testSearch = async (title, author, genre) => {
-    let fetchURL = "http://localhost:8000/books/search?";
-
-    if (title !== undefined) {
-        fetchURL += `&title=${title}`;
-    }
-    if (author !== undefined) {
-        fetchURL += `&author=${author}`;
-    }
+    useEffect(() => {
+        search(undefined, undefined, ["Horror"]);
+    }, []);
     
-    if (genre !== undefined) {
-        for (let i = 0; i < genre.length; i++) {
-            fetchURL += `&genre[${i}]=${genre[i]}`;
+
+    async function search(title, author, genre) {
+        let fetchURL = "http://localhost:8000/books/search?";
+    
+        if (title !== undefined) {
+            fetchURL += `&title=${title}`;
         }
+        if (author !== undefined) {
+            fetchURL += `&author=${author}`;
+        }
+        
+        if (genre !== undefined) {
+            for (let i = 0; i < genre.length; i++) {
+                fetchURL += `&genre[${i}]=${genre[i]}`;
+            }
+        }
+    
+        const response = await axios.get(fetchURL).catch((error) => console.log(error));
+        setData(response.data);
     }
 
-    const response = await axios.get(fetchURL).catch((error) => console.log(error));
-    console.log(response);
-}
-*/
+    Within return statement: <p>{data.books !== undefined && data.books[0].title}</p>
+
+
+    POKEMON API EXAMPLE
+    ---
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        async function search(limit, offset) {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`).catch((err) => console.log(err));
+            setData(response.data);
+        }
+        search(5, 0);
+    }, []);
+
+
+    Within render: <p>{(data.results !== undefined) ? data.results[0].name : "Not yet"}</p>
+    */
