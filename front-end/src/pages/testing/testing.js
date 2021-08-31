c643b4e76e9b2a4ad97424c2e03bdc386913e7e5
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-08-31 15:26:18 -0700

    Endpoint testing with React Hooksq
    Finished testing with Pokemon API, now moving to test with our book server.

--- front-end/src/pages/testing/testing.js
+++ front-end/src/pages/testing/testing.js
@@ -1,25 +1,23 @@
-    /*
-    // Just testing my searching capabilities
-    const testSearch = async (title, author, genre) => {
-        let fetchURL = "http://localhost:8000/books/search?";
+/*
+import axios from 'axios';
 
-        if (title !== undefined) {
-            fetchURL += `&title=${title}`;
-        }
-        if (author !== undefined) {
-            fetchURL += `&author=${author}`;
-        }
-        
-        if (genre !== undefined) {
-            for (let i = 0; i < genre.length; i++) {
-                fetchURL += `&genre[${i}]=${genre[i]}`;
-            }
-        }
+const testSearch = async (title, author, genre) => {
+    let fetchURL = "http://localhost:8000/books/search?";
 
-        const response = await axios.get(fetchURL).catch((error) => console.log(error));
-        console.log(response);
-
+    if (title !== undefined) {
+        fetchURL += `&title=${title}`;
+    }
+    if (author !== undefined) {
+        fetchURL += `&author=${author}`;
+    }
+    
+    if (genre !== undefined) {
+        for (let i = 0; i < genre.length; i++) {
+            fetchURL += `&genre[${i}]=${genre[i]}`;
+        }
     }
 
-    // await testSearch();
-    */
+    const response = await axios.get(fetchURL).catch((error) => console.log(error));
+    console.log(response);
+}
+*/
