8648041ee4591036ce8a4358bd7225aa363d4c0d
nith2001 <75632283+nith2001@users.noreply.github.com>
2021-09-03 20:02:04 -0700

    Update BookPage.js
    Wrote some rough draft code in the book page to fetch data from the "Get Detailed Information for a Single Book" endpoint. Still need to work on making sure it'll properly render. I am unable to test this code directly, so I'll probably have to rely on Sid or Elliot to help with that.

--- front-end/app/src/pages/book-page/BookPage.js
+++ front-end/app/src/pages/book-page/BookPage.js
@@ -1,49 +1,64 @@
 import "./style.css";
-// import axios from 'axios'
-import React, {useState} from 'react';
+import axios from 'axios'
+import React, {useState, useEffect} from 'react';
 
-export default function BookPage() {
+export default function BookPage(ISBN) {
     
-    const [book, setBook] = useState({
-      title: "TitleTest",
-      authors: "AuthorTest",
-      genres: "Horror",
-      description: "lolololololololololol",
-    });
+  const [title, setTitle] = useState("");
+  const [authors, setAuthors] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState([]);
     
-    // toggleBook = async () => {
-    //   let response = await axios.get("localhost:8000/books/detail/" + num).catch((err) => console.log(err));
-    //   setBook(response.data);
-    // } 
-    // let test = await toggleBook();
-    // console.log(book);
+  useEffect(() => {
+    getBookData(ISBN);
+  }, []);
+    
+  async function getBookData(isbnParam) {
+    // expecting this base URL to change
+    let fetchURL = "http://localhost:8000/books/detail/";
+  
+    if (isbn !== undefined) {
+      fetchURL += `${isbnParam}`;
+    }
 
-    return(
-      
-      <div id="bookpage-container">
-        <div id="left-column">
-          <div id="imagebox"></div>
-          <div id="bookstand-selectors">
-            <select id="selector">
-              <option className="opt">Choose Shelf</option>
-              <option className="opt">Plan to Read</option>
-              <option className="opt">Currently Reading</option>
-              <option className="opt">Finished</option>
-            </select>
-            <button id="add-button">ADD TO BOOKSTAND</button>
-          </div>
-        </div>
+    const response = await axios.get(fetchURL).catch((error) => console.log(error));
+    
+    setTitle(response.data.title);
+    setAuthors(response.data.authors);
+    setGenres(response.data.genres);
+    setDate(response.data.datePublished);
+    setDescription(response.data.description);
+  }
 
-        <div id="right-column">
-          <h1>Title: {book.title}</h1>
-          <hr />
-          <p><strong>Author(s): </strong>{book.authors}</p>
-          <p><strong>Genre(s): </strong>{book.genres}</p>
-          <p><strong>Description:</strong></p>
-          <p>{book.description}</p>
+  return( 
+    <div id="bookpage-container">
+      <div id="left-column">
+        <div id="imagebox"></div>
+        <div id="bookstand-selectors">
+          <select id="selector">
+            <option className="opt">Choose Shelf</option>
+            <option className="opt">Plan to Read</option>
+            <option className="opt">Currently Reading</option>
+            <option className="opt">Finished</option>
+          </select>
+          <button id="add-button">ADD TO BOOKSTAND</button>
         </div>
       </div>
-    );
+
+      <div id="right-column">
+        <h1>Title: {title}</h1>
+        {/* TODO: The code for inserting these parameters are incorrect and will paste before
+        fully loading. Need to fix that. Reference the testing folder code. */}
+        <hr />
+        <p><strong>Author(s): </strong>{authors}</p>
+        <p><strong>Genre(s): </strong>{genres}</p>
+        <p><strong>Date Published:</strong>{date}</p>
+        <p><strong>Description:</strong></p>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
 
 /*   reminder from api doc that bookdata will come as
@@ -55,5 +70,3 @@ export default function BookPage() {
     "description": "Katniss Everdeen fights the distopian government"
   }
 */
-
-
