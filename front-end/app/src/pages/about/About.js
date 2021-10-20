8b5c8ea5da15cf7b196f5c3e0a757905c2e2df67
nith2001 <nith2001@uw.edu>
2021-10-20 16:40:34 -0700

    update

--- front-end/app/src/pages/about/About.js
+++ front-end/app/src/pages/about/About.js
@@ -1,6 +1,4 @@
 import "./index.css";
-import React, { useState, useEffect } from 'react';
-
 
 export default function AboutPage() {
 
--- front-end/app/src/pages/book-stand/BookStand.js
+++ front-end/app/src/pages/book-stand/BookStand.js
@@ -1,6 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
 export default function BookStandPage() {
 
-   // const [bookStand, setBookStand] = useState({/* needs setup */});
+  const [bookStand, setBookStand] = useState({/* needs setup */});
 
   return (
     <div>Book stand</div>
