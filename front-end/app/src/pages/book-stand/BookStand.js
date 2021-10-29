f9ac36a1e28e8c643479328e1cf04faf449f6769
nith2001 <nith2001@uw.edu>
2021-10-28 20:53:11 -0700

    wrote plan for the bookstand page

--- front-end/app/src/pages/book-stand/BookStand.js
+++ front-end/app/src/pages/book-stand/BookStand.js
@@ -4,6 +4,13 @@ export default function BookStandPage() {
 
   const [bookStand, setBookStand] = useState({/* needs setup */});
 
+  // plan: get the username from the client by getting it from their cookie
+  // with "http://localhost:8000/grab/username". Using that username, we get
+  // the bookshelves via bookshelves/get/:username/:(optional)bookshelf,
+  // which returns a list of 3 objects, one object per shelf type
+  // implement the scroll function used in the browse page and use the BookCard
+  // set up by Audrey to describe each shelf.
+
   return (
     <div>Book stand</div>
   );
