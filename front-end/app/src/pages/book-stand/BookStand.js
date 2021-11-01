ff8383fd204f410373b8226e918f3a0eb4b7c0d2
nith2001 <nith2001@uw.edu>
2021-11-01 14:53:47 -0700

    Update BookStand.js

--- front-end/app/src/pages/book-stand/BookStand.js
+++ front-end/app/src/pages/book-stand/BookStand.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function BookStandPage() {
 
-  const [bookStand, setBookStand] = useState({/* needs setup */});
+  const [bookStand, setBookStand] = useState({
+    bookShelves: [],
+  });
+  const GRAB_COOKIE_URL = "http://localhost:8000/grab/username";
+  const SHELVES_URL = "http://localhost:8000/get/";
+  let returnToLogin = false;
+  let errorPage = false;
 
   // plan: get the username from the client by getting it from their cookie
   // with "http://localhost:8000/grab/username". Using that username, we get
@@ -11,8 +18,50 @@ export default function BookStandPage() {
   // implement the scroll function used in the browse page and use the BookCard
   // set up by Audrey to describe each shelf.
 
-  return (
-    <div>Book stand</div>
-  );
+  // calls the the book constructor ONCE
+  useEffect(() => {
+    getShelves();
+  }, []);
+
+  async function getShelves() {
+    const USERNAME = await axios.get(GRAB_COOKIE_URL)
+      .then((res) => {return res.username})
+      .catch((res) => console.error(res.error));
+
+    // TODO: test if it's properly accounting for errors that can arise from this endpoint call,
+    // such as cookie expiration or error codes.
+    let shelfList = undefined;
+    if (USERNAME !== undefined) {
+      shelfList = await axios.get(`${SHELVES_URL}${USERNAME}`)
+        .catch((res) => console.error(res.error));
+    } else {
+      returnToLogin = true;
+    }
+
+    // TODO: test if it's properly accounting for errors that can arise from this endpoint call,
+    // such asr error codes.
+    if (shelfList !== undefined) {
+      setBookStand(shelfList);
+    } else {
+      errorPage = true;
+    }
+  }
+
+  // decides what to show on the screen
+  if (returnToLogin) {
+    // need to log out the user using post request with logout URL
+    // then return the login/signup page
+    return (
+      <Form />
+    );
+  } else if (errorPage) {
+    return (
+      <p>Error! Check console!</p>
+    )
+  } else {
+    return (
+      <div>Book stand</div>
+    );
+  }
 }
 
