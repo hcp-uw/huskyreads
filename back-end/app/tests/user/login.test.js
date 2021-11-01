c2aba27ce5d70edaa84732604d8d75073f2525e6
Sidharth Lakshmanan <sidharth.m.lakshmanan@gmail.com>
2021-10-31 21:38:21 -0700

    Add login tests

--- back-end/app/tests/user/login.test.js
+++ back-end/app/tests/user/login.test.js
@@ -3,73 +3,82 @@ var server = global.server;
 var chai = global.chai;
 
 
-describe('POST /signup', function() {
+initialize = async (done) => {
+    let user1 = {
+        username: "elliot",
+        password: "pass1"
+    }
+    let user2 = {
+        username: "frank",
+        password: "pass2"
+    }
+    let user3 = {
+        username: "vikram",
+        password: "pass1"
+    }
+    await global.resetDB(() => {});
+    await chai.request(server).post('/signup').send(user1);
+    await chai.request(server).post('/signup').send(user2);
+    await chai.request(server).post('/signup').send(user3);
+    done();
+}
+
+describe('POST /login', function() {
     before(function(done) {
-        global.resetDB(done);
+        initialize(done);
     })
     
-    it('200: signup user', function(done) {
+    it('200: login user', function(done) {
         let user = {
             username: "elliot",
             password: "pass1"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
             res.should.have.status(200);
-            res.text.should.be.equal.toString("Signup Successful");
+            res.text.should.be.equal.toString("Login Successful");
             done();
         })
     })
-    it('200: signup second user', function(done) {
+
+    it('200: login second user', function(done) {
         let user = {
             username: "frank",
             password: "pass2"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
             res.should.have.status(200);
-            res.text.should.be.equal.toString("Signup Successful");
+            res.text.should.be.equal.toString("Login Successful");
             done();
         })
     })
-    it('200: signup third user', function(done) {
+
+    it('200: login third user', function(done) {
         let user = {
             username: "vikram",
             password: "pass1"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
             res.should.have.status(200);
-            res.text.should.be.equal.toString("Signup Successful");
+            res.text.should.be.equal.toString("Login Successful");
             done();
         })
     })
-    it('400: username already exists', function(done) {
-        let user = {
-            username: "frank",
-            password: "pass1"
-        }
-        chai.request(server)
-        .post('/signup')
-        .send(user)
-        .end(function(err, res) {
-            res.should.have.status(400);
-            res.text.should.be.equal.toString("Username already taken");
-            done();
-        })
-    })
+
     it('400: missing username', function(done) {
         let user = {
             password: "pass1"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
             res.should.have.status(400);
@@ -77,12 +86,13 @@ describe('POST /signup', function() {
             done();
         })
     })
-    it('400: missing password', function(done) {
+
+    it('400: missing username', function(done) {
         let user = {
-            username: "frank",
+            username: "vikram"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
             res.should.have.status(400);
@@ -90,11 +100,12 @@ describe('POST /signup', function() {
             done();
         })
     })
+
     it('400: missing username and password', function(done) {
         let user = {
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
             res.should.have.status(400);
@@ -102,45 +113,48 @@ describe('POST /signup', function() {
             done();
         })
     })
-    it('400: empty username', function(done) {
+
+    it('401: invalid username', function(done) {
         let user = {
-            username: "",
+            username: "vik",
             password: "pass1"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
-            res.should.have.status(400);
-            res.text.should.be.equal.toString("Missing username or password");
+            res.should.have.status(401);
+            res.text.should.be.equal.toString("Invalid user credentials");
             done();
         })
     })
-    it('400: empty password', function(done) {
+
+    it('401: invalid password', function(done) {
         let user = {
-            username: "frank",
-            password: ""
+            username: "vikram",
+            password: "pass123"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
-            res.should.have.status(400);
-            res.text.should.be.equal.toString("Missing username or password");
+            res.should.have.status(401);
+            res.text.should.be.equal.toString("Invalid user credentials");
             done();
         })
     })
-    it('400: empty username and password', function(done) {
+
+    it('401: invalid username and password', function(done) {
         let user = {
-            username: "",
-            password: ""
+            username: "vik",
+            password: "pass123"
         }
         chai.request(server)
-        .post('/signup')
+        .post('/login')
         .send(user)
         .end(function(err, res) {
-            res.should.have.status(400);
-            res.text.should.be.equal.toString("Missing username or password");
+            res.should.have.status(401);
+            res.text.should.be.equal.toString("Invalid user credentials");
             done();
         })
     })
