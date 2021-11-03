24606e761cfd4988d540b8525642123fc998fbc8
nith2001 <nith2001@uw.edu>
2021-11-03 15:43:26 -0700

    Update BookStand.js

--- front-end/app/src/pages/book-stand/BookStand.js
+++ front-end/app/src/pages/book-stand/BookStand.js
@@ -15,7 +15,7 @@ export default function BookStandPage() {
   // the bookshelves via bookshelves/get/:username/:(optional)bookshelf,
   // which returns a list of 3 objects, one object per shelf type
   // implement the scroll function used in the browse page and use the BookCard
-  // set up by Audrey to describe each shelf.
+  // set up by Audrey to display each book under each bookshelf.
 
   // calls the the book constructor ONCE
   useEffect(() => {
