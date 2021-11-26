2cad26548f7ea3efdda83af57d9f2e9d60a8371f
Nicholas Boren <nboren@cs.washington.edu>
2021-11-26 00:18:13 -0800

    feat: processed JSON data now follows good style

--- back-end/scripts/createJsonFile.py
+++ back-end/scripts/createJsonFile.py
@@ -6,19 +6,20 @@ original dirty book data
 
 import re
 import os
+import json
 
 #TODO(nboren@uw.edu): Change this global variable not to be dependent on my machine.
-PROCESSED_DATA_PATH = "/Users/nboren/huskyreads/back-end/data/processed/"
+PROCESSED_DATA_PATH = "/Users/nboren/personal/huskyreads/back-end/data/processed/"
 
 def text_cleaner(current_line: str) -> str:
-    """ Removes text not within the bounds of curly braces
+    """ Removes the current line into cleaned JSON text
 
         Args:
             current_line:
                 The current line being looked at
 
         Returns:
-            The processed line from the book data
+            The processed and correctly formatted JSON line
     """
     regex_result = re.search("{.*", current_line)
     return regex_result[0]
@@ -46,26 +47,21 @@ def append_multiple_lines(lines_to_append: str, output_path=os.path.join(PROCESS
             The location of where the processed output will go.
             Defaults to a text file in data/processed called processed_output.
     """
+
+    books = list()
+    for line in lines_to_append:
+        cleaned_line = text_processer(line)
+        books.append(cleaned_line)
+        
     with open(output_path, 'a+', encoding='utf-8') as f_out:
-        append_at_end = False
-        f_out.seek(0)
-        data = f_out.read(100)
-        if len(data) > 0:
-            append_at_end = True
-
-        for line in lines_to_append:
-            if append_at_end:
-                f_out.write('\n')
-            else:
-                append_at_end = True
-
-            cleaned_line = text_cleaner(line)
-            f_out.write(cleaned_line)
+        json_result = json.dumps({'books': books}, indent=4, sort_keys = False) 
+        f_out.write(json_result)
 
 def main():
-    file_path = "/Users/nboren/huskyreads/back-end/data/raw/sample_data.txt"
+    file_path = "/Users/nboren/personal/huskyreads/back-end/data/raw/sample_data.txt"
     file_lines = parse_text_file(file_path)
     append_multiple_lines(file_lines)
 
 if __name__ == '__main__':
     main()
+
