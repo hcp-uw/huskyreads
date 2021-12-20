a8ca3b16088f56c26f9c4165ea85571497d7760a
Audrey Kho <audreyckho@gmail.com>
2021-12-19 23:10:23 -0800

    nav + book stand css changes

--- front-end/app/src/components/book-card/BookCard.js
+++ front-end/app/src/components/book-card/BookCard.js
@@ -1,11 +1,11 @@
 import './index.css';
 
-export default function BookCard({img, title, author}) {
+export default function BookCard({img, title, authors, isbn}) {
   return (
-    <a className="book-list_card" href="/browse">
+    <a id={isbn} className="book-list_card" href="/">
       <img className="book-card_img" src={img} alt={title}/>
       <h4 className="book-card_title">{title}</h4>
-      <p className="book-card_author">{author}</p>
+      <p className="book-card_author">{authors}</p>
     </a>
   );
 }
