fa5464d859a4b6994bde76c0f8e16521d4f4b2ca
ASimpleFox <asimplefox@gmail.com>
2021-12-29 16:49:21 -0800

    Finished modifying and testing all endpoints

--- back-end/scripts/endpoints.sql
+++ back-end/scripts/endpoints.sql
@@ -20,7 +20,7 @@ AND Users.password = [Password]
 /* Create new user */
 /* Parameters: Username, Password */
 /* Try-Catch this since username needs to be unique! (User.username is a unique field) */
-INSERT INTO Users (username, password) VALUES ([Username], [Password]);
+INSERT INTO Users(username, password) VALUES ([Username], [Password]);
 SELECT LAST_INSERT_ID() AS userid;  /* Gets the user's ID after insert, for bookshelves */
 INSERT INTO Bookshelves (userid, shelf_name) VALUES
     (id_user, "reading"),
@@ -38,20 +38,23 @@ WHERE username = [Username]
 ;
 
 
+-- TESTED
 /* Accessing User Bookshelves */
-/* Valid Bookshelf Names: "reading", "read", "want_to_read" */
-/* Parameters: User_id, Username, BookshelfName */
+/* Valid Bookshelf Names (for now): "reading", "read", "want_to_read" */
+/* Parameters: User_id, BookshelfName */
+DROP TABLE IF EXISTS Shelves, Book_Data;     /* Clearing old temporary tables */
 CREATE TEMPORARY TABLE Shelves
-    SELECT Bookshelf.ISBN AS ISBN, Bookshelf.shelf_name AS name
-    FROM Bookshelf
-    INNER JOIN Users
-        ON Users.id = Bookshelf.id_user
-    WHERE Bookshelf.shelf_name = [BookshelfName] AND Users.id = [User_id]
+    SELECT Bookshelves.shelf_name AS shelfname, Bookshelf_Books.ISBN AS ISBN
+    FROM Bookshelves
+    INNER JOIN Bookshelf_Books
+        ON Bookshelves.id = Bookshelf_Books.id_bookshelf
+    WHERE Bookshelves.shelf_name = [BookshelfName]
+    AND Bookshelves.id_user = [User_id]
 ;
 CREATE TEMPORARY TABLE Book_Data
-	SELECT Shelves.name AS shelfname, Books.title AS title, Books.ISBN AS ISBN
+	SELECT Shelves.shelfname AS shelfname, Books.title AS title, Books.ISBN AS ISBN
   	FROM Books
-  	RIGHT JOIN Shelves
+  	INNER JOIN Shelves
       	ON Shelves.ISBN = Books.ISBN
 ;
 SELECT Book_Data.shelfname, Book_Data.title, Book_Data.ISBN,
@@ -59,9 +62,9 @@ SELECT Book_Data.shelfname, Book_Data.title, Book_Data.ISBN,
     GROUP_CONCAT(DISTINCT Genres.name SEPARATOR ',') AS genres
 FROM Book_Data
 INNER JOIN Book_Authors
-    ON Book_Data.ISBN = Book_Authors.ISBN
+    ON Book_Data.ISBN = Book_Authors.ISBN_book
 INNER JOIN Book_Genres
-    ON Book_Data.ISBN = Book_Genres.ISBN
+    ON Book_Data.ISBN = Book_Genres.ISBN_book
 INNER JOIN Authors
     ON Book_Authors.id_author = Authors.id
 INNER JOIN Genres
@@ -73,12 +76,12 @@ GROUP BY Book_Data.shelfname, Book_Data.title, Book_Data.ISBN
 -- TESTED
 /* Add Book to Bookshelf */
 /* Parameters: User_id, Bookshelf, ISBN */
-SELECT Bookshelves.id AS bookshelf_id
+SELECT Bookshelves.id AS id_bookshelf
 FROM Bookshelves
 WHERE Bookshelves.id_user = [User_id]
 AND Bookshelves.shelf_name = [Bookshelf]
 ;
-INSERT INTO Bookshelf_Books VALUES (bookshelf_id, [ISBN]);
+INSERT INTO Bookshelf_Books VALUES (id_bookshelf, [ISBN]);
 
 
 -- TESTED
@@ -95,50 +98,44 @@ AND Bookshelf_Books.ISBN = [ISBN]
 ;
 
 
+-- TESTED
 /* Retrieving Book Data */
 /* Parameters: Title, Author, Genre ARR, Offset, ResultLength */
-CREATE TEMPORARY TABLE Results
-    SELECT Books.title AS title, Authors.name AS author_name
-    FROM Books
-    INNER JOIN Book_Authors
-        ON Books.ISBN = Book_Authors.ISBN
-    INNER JOIN Authors
-        ON Book_Authors.id_author = Authors.id
-    INNER JOIN Book_Genres
-        ON Books.ISBN = Book_Genres.ISBN
-    INNER JOIN Genres
-        ON Book_Genres.id_genre = Genres.id
-    WHERE Books.title = [Title]
-    AND Genres.name = [Genres] /* If multiple genres in array: use OR statements */
-    ;
-SELECT Results.title, GROUP_CONCAT(DISTINCT Results.author_name SEPARATOR ',') AS authors
-FROM Results
-GROUP BY Results.title
-HAVING FIND_IN_SET([Author], authors)
-LIMIT [ResultLength] /* Default value 10 */
+SELECT Books.title AS title,
+    GROUP_CONCAT(DISTINCT Authors.name SEPARATOR ',') AS author_names
+FROM Books
+INNER JOIN Book_Authors
+    ON Books.ISBN = Book_Authors.ISBN_book
+INNER JOIN Authors
+    ON Book_Authors.id_author = Authors.id
+INNER JOIN Book_Genres
+    ON Books.ISBN = Book_Genres.ISBN_book
+INNER JOIN Genres
+    ON Book_Genres.id_genre = Genres.id
+WHERE Books.title = [Title]
+AND Genres.name = [Genre] -- If multiple genres, use OR statements
+GROUP BY Books.title
+HAVING FIND_IN_SET([Author], author_names)
+LIMIT [ResultLength]
 OFFSET [Offset]
 ;
 
 
+-- TESTED
 /* Get Detailed Information for Single Book */
 /* Parameter: ISBN */
-CREATE TEMPORARY TABLE Results
-    SELECT Books.title AS title, Books.date_published AS date_published,
-        Authors.name AS author_name, Genres.name AS genre_name
-    FROM Books
-    INNER JOIN Book_Authors
-        ON Books.ISBN = Book_Authors.ISBN
-    INNER JOIN Authors
-        ON Book_Authors.id_author = Authors.id
-    INNER JOIN Book_Genres
-        ON Books.ISBN = Book_Genres.ISBN
-    INNER JOIN Genres
-        ON Book_Genres.id_genre = Genres.id
-    WHERE Books.ISBN = [ISBN]
-    ;
-SELECT Results.title, Results.date_published,
-    GROUP_CONCAT(DISTINCT Results.author_name SEPARATOR ',') AS authors,
-    GROUP_CONCAT(DISTINCT Results.genre_name SEPARATOR ',') AS genres
-FROM Results
-GROUP BY Results.title, Results.date_published
+SELECT Books.title AS title, Books.date_published AS date_published,
+    GROUP_CONCAT(DISTINCT Authors.name SEPARATOR ',') AS authors,
+    GROUP_CONCAT(DISTINCT Genres.name SEPARATOR ',') AS genres
+FROM Books
+INNER JOIN Book_Authors
+    ON Books.ISBN = Book_Authors.ISBN_book
+INNER JOIN Authors
+    ON Book_Authors.id_author = Authors.id
+INNER JOIN Book_Genres
+    ON Books.ISBN = Book_Genres.ISBN_book
+INNER JOIN Genres
+    ON Book_Genres.id_genre = Genres.id
+WHERE Books.ISBN = [ISBN]
+GROUP BY Books.title, Books.date_published
 ;
