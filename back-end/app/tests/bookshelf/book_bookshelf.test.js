ab45187e2b14ceef41a16876f5c4f640248f6bf2
Elliot Schumacher <Elliot.l.schumacher.code@gmail.com>
2022-03-09 00:00:08 -0800

    Created test case for book in all default shelves

--- back-end/app/tests/bookshelf/book_bookshelf.test.js
+++ back-end/app/tests/bookshelf/book_bookshelf.test.js
@@ -32,6 +32,21 @@ initialize = async (done) => {
         isbn: 3333333333,
     }
     let task4 = {
+        username: "elliot",
+        bookshelf: "want_to_read",
+        isbn: 4444444444,
+    }
+    let task5 = {
+        username: "elliot",
+        bookshelf: "read",
+        isbn: 4444444444,
+    }
+    let task6 = {
+        username: "elliot",
+        bookshelf: "reading",
+        isbn: 4444444444,
+    }
+    let task7 = {
         username: "frank",
         bookshelf: "want_to_read",
         isbn: 5555555555,
@@ -44,6 +59,9 @@ initialize = async (done) => {
     await chai.request(server).post('/bookshelves/add').send(task2);
     await chai.request(server).post('/bookshelves/add').send(task3);
     await chai.request(server).post('/bookshelves/add').send(task4);
+    await chai.request(server).post('/bookshelves/add').send(task5);
+    await chai.request(server).post('/bookshelves/add').send(task6);
+    await chai.request(server).post('/bookshelves/add').send(task7);
     done();
 }
 
@@ -105,6 +123,19 @@ describe('GET /bookshelves/book/:username/:isbn', function() {
             res.body.length.should.be.eql(2);
             res.body[0].should.be.eql("reading");
             res.body[1].should.be.eql("read");
+            done();
+        });
+    });
+
+    it('200: get bookshelves when user has 3 bookshelf with book', function(done) {
+        chai.request(server)
+        .get('/bookshelves/book/elliot/4444444444')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            res.body.length.should.be.eql(3);
+            res.body[0].should.be.eql("want_to_read");
+            res.body[1].should.be.eql("read");
+            res.body[2].should.be.eql("reading");
             done();
         });
     });
