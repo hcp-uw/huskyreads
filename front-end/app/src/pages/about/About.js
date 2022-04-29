3d041839a525d36107aef821833efd7aec3c9b2c
Juda <toadsmen@uw.edu>
2022-04-28 18:38:39 -0700

    removed Sriram card

--- front-end/app/src/pages/about/About.js
+++ front-end/app/src/pages/about/About.js
@@ -59,18 +59,6 @@ export default function AboutPage() {
                         </p>
                     </div>
                     <div className="member-box">
-                        <img src="/images/cutepfp.png"
-                             alt="pic"
-                             className="member-pic" />
-                        <h3>Sriram Thothathri</h3>
-                        <p className="membertext">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
-                    <div className="member-box">
                         <img src="/club-member-pics/elliot-pic.png"
                              alt="pic"
                              className="member-pic" />
